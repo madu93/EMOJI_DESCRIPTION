0a16ca98b3c81bc3421a355d7f330312405b72fa
madu93 <madu93.gunarathna@gmail.com>
2022-08-23 00:29:50 -0700

    COMMIT1
    COMMIT

---
+++ ReactProject/src/emojipedia.js
@@ -0,0 +1,190 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "🧐",
+    name: " Face With Monocle",
+    meaning:
+      "A monocle is a visual aid with only one glass."
+  },
+
+  {
+    id: 5,
+    emoji: "🤨",
+    name: " Face With a Raised Eyebrow",
+    meaning:
+      "Can be used to express skepticism, disbelief or disapproval."
+  },
+  {
+    id: 6,
+    emoji: "🤩",
+    name: " Smirking Face With Starry Eyes",
+    meaning:
+      "This is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+   {
+    id: 7,
+    emoji: "😱",
+    name: "  Face Screaming in Fear",
+    meaning:
+      "Horror-stricken face with two hands, screaming. Is scared stiff, feeling nothing but fear and panic. The creepy emoji can also be used ironically or as a symbol for Halloween."
+  },
+   {
+    id: 8,
+    emoji: "🤗 ",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  },
+   {
+    id: 9,
+    emoji: "🤔",
+    name: "  Thinking Face",
+    meaning:
+      "This is very excited about something and already full of anticipation. Overwhelmed and speechless after meeting someone you like a lot. Fascinated by the glory and the world of the stars."
+  },
+   {
+    id: 10,
+    emoji: "🤭",
+    name: "Face With Hand Over The Mouth",
+    meaning:
+      "Expresses shock, surprise, or a sudden perception. Typical gesture, which shows that the other person is not telling the truth. Thinks he's being lied to."
+  },
+  {
+    id:  11,
+    emoji: "🤧",
+    name: "  Sneezing Face",
+    meaning:
+      "A sneezing face with eyes closed, blowing its nose into a handkerchief. I caught a cold! Shows that someone is ill or feels uncomfortable."
+  },
+  {
+    id: 12,
+    emoji: "🤮",
+    name: " Face With Open Mouth, Throwing Up",
+    meaning:
+      "I could almost throw up. Might show how drunk the person was last night. The puking smiley can also state what you think of something."
+  },
+  {
+    id: 13,
+    emoji: "🤠",
+    name: " Face With Cowboy Hat",
+    meaning:
+      "Howdy! Typical American smiley with cowboy hat from the Wild West. Stands for freedom, nature, and hard work."
+  },
+  {
+    id: 14,
+    emoji: "😈",
+    name: " Smiling Face With Horns",
+    meaning:
+      "Mischievously grinning goblin smiley with small devil horns. Wants to cause trouble, is a real teaser or planning something ugly just now."
+  },
+  {
+    id: 15,
+    emoji: "👻",
+    name: "Ghost",
+    meaning:
+      "The funny-smiling ghost is too cute to frighten others or cause harm. Symbol for Halloween or jokingly for creepy things, e.g. movies."
+  },
+  {
+    id: 16,
+    emoji: "🎃",
+    name: " Pumpkin lantern",
+    meaning:
+      "Trick or treat! A candle is placed in a hollowed-out pumpkin with a grimace. The Halloween tradition was brought to the US by Irish immigrants. The emoji is usually sent as a symbol for Halloween."
+  },
+  {
+    id: 17,
+    emoji: "🤲",
+    name: " Holding hands up, palms against each other",
+    meaning:
+      "I'm praying that everything will go smoothly today or “Can you lend me some money please?“ The upturned palms represent a form of praying or asking for handouts. In American sign language, the symbol of an open book."
+  },
+  {
+    id: 18,
+    emoji: "🙌",
+    name: "  Person Raising Both Hands In Celebration",
+    meaning:
+      "Hallelujah! Hands are stretched upwards to celebrate. Is having a party, dancing wildly and friskily. Is in a good mood or having a lot of fun. Can also stand for “Banzai”, a Japanese cheer that brings luck and joy for 10,000 years."
+  },
+  {
+    id: 19,
+    emoji: "🤝",
+    name: " Handshake",
+    meaning:
+      "Shake on it! The handshake can stand for a greeting or farewell, a sign of agreement or for a deal. Two people are holding hands or, in a figurative sense, to give someone a helping hand."
+  },
+  {
+    id: 20,
+    emoji: "🤙",
+    name: " Call Me” Hand Sign",
+    meaning:
+      "The closed fist with abducted thumb and little finger has a phone-like shape. “Call me“ or “Let's have a call soon“. Similar to the surfer greeting “Hang loose“."
+  },
+  {
+    id: 21,
+    emoji: "✍",
+    name: "  Writing Hand",
+    meaning:
+      "Right hand writing with a pen. Is related to writing. I'll contact you and write a message to you. There will be a test in school. You're writing a poem for your honey."
+  },
+  {
+    id: 22,
+    emoji: "👌",
+    name: " Ok Hand Sign",
+    meaning:
+      "Thumb and forefinger form an O. Symbol for “Okay!“ Sign of approval, agreement or that everything is okay. Careful, in some countries it is considered offensive and insulting (“you a**hole“)."
+  },
+  {
+    id: 23,
+    emoji: "👋",
+    name: " Waving Hand",
+    meaning:
+      "Hello and goodbye! Have a good trip! A friendly waving hand. Can be used as a welcome or farewell. Or sarcastically, if you wished someone or something would disappear."
+  },
+  {
+    id: 24,
+    emoji: "🤚",
+    name: "  Raised Back of Hand",
+    meaning:
+      "A raised hand, which is shown in reverse position (back of the hand is in the front). Someone wants to draw attention to himself, e.g. to ask something or to signalize his participation."
+  },
+  {
+    id: 25,
+    emoji: "🤏",
+    name: "  Pinching hand",
+    meaning:
+      "Just this tiny bit is missing! Thumb and forefinger are close together to grab a small object. Indication that something is small or only available in small quantities."
+  },
+    {
+    id: 26,
+    emoji: "👂",
+    name: "  Ear",
+    meaning:
+      "The ear emoji can mean “I can hear something” or “You have my full attention”. The ear is also one of the erotic zones of man and woman"
+  }
+];
+
+export default emojipedia;
+
+  
